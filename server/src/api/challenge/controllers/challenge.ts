0692f45310d80aaabce308899f4405eb1199b6ac
Himel Majumder <himelmajumder1@gmail.com>
2025-02-12 23:07:28 +0100

    ✨feat: implement custom findOne method in challenge controller

--- server/src/api/challenge/controllers/challenge.ts
+++ server/src/api/challenge/controllers/challenge.ts
@@ -4,4 +4,15 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::challenge.challenge');
+export default factories.createCoreController('api::challenge.challenge', ({ strapi }) => ({
+    async findOne(ctx) {
+
+        const { id: slug } = ctx.params;
+
+        const data = await strapi.db.query('api::challenge.challenge').findOne({ where: { slug } });
+
+        const results = await this.sanitizeOutput(data, ctx);
+
+        return results;
+    },
+}));

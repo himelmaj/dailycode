c5a8dccace5ab94b3c71e2065b63956513a2aa04
Himel Majumder <himelmajumder1@gmail.com>
2025-02-13 02:30:05 +0100

    ✨add slug validation and custom route for challenge retrieval

--- server/src/api/challenge/controllers/challenge.ts
+++ server/src/api/challenge/controllers/challenge.ts
@@ -7,9 +7,17 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::challenge.challenge', ({ strapi }) => ({
     async findOne(ctx) {
 
-        const { id: slug } = ctx.params;
+        const { slug } = ctx.params;
+
+        if (!/^(day\d{1,5})$/.test(slug)) {
+            return ctx.badRequest('Invalid slug format');
+        }
 
         const data = await strapi.db.query('api::challenge.challenge').findOne({ where: { slug } });
+
+        if (!data) {
+            return ctx.notFound("Challenge not found");
+        }
 
         const results = await this.sanitizeOutput(data, ctx);
 
---
+++ server/src/api/challenge/routes/01-custom-challenge.ts
@@ -0,0 +1,10 @@
+
+export default {
+    routes: [
+        {
+            method: 'GET',
+            path: '/challenges/:slug(day\\d{1,5})',
+            handler: 'challenge.findOne',
+        }
+    ]
+}
